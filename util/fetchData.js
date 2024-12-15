e850730918de334fe4db1cc102e25f66b2decb08
carrolldominic <carroll.886@osu.edu>
2024-12-14 22:19:26 -0500

    fix 2

--- util/fetchData.js
+++ util/fetchData.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 async function fetchData(url) {
     try {
         let ran = Math.ceil(Math.random()*1000+200);
-        let agent = 'SmartFilings' + ran + ' smart-filings.vercel.app/';
+        let agent = 'DCC' + ran + ' dominiccarroll.com';
         const response = await axios.get(url, {
             headers: {
               'User-Agent': agent
             }
-          });        return response.data;
+          });
+          
+        return response.data;
     } catch (error) {
         console.error('Error fetching the URL:', error);
         return error;
