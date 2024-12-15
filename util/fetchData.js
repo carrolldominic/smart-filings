0f6bea737e915fc81a80d0e631da8cc3e0c46c90
carrolldominic <carroll.886@osu.edu>
2024-12-15 00:13:09 -0500

    fetch data module

--- util/fetchData.js
+++ util/fetchData.js
@@ -1,20 +1,16 @@
 const axios = require('axios');
-
 async function fetchData(url) {
     try {
         let ran = Math.ceil(Math.random()*1000+200);
-        let agent = 'DCC' + ran + ' dominiccarroll.com';
+        let agent = 'Carroll' + ran + ' info@dominiccarroll.com';
         const response = await axios.get(url, {
             headers: {
               'User-Agent': agent
             }
-          });
-          
-        return response.data;
+          });        return response.data;
     } catch (error) {
         console.error('Error fetching the URL:', error);
         return error;
       }
 }
-
 module.exports = fetchData;
