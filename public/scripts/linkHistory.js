6bf318060ad1fa5c2a7dc3c08a1d44db17c0b960
carrolldominic <carroll.886@osu.edu>
2024-12-21 21:55:43 -0500

    debug

--- public/scripts/linkHistory.js
+++ public/scripts/linkHistory.js
@@ -1,4 +1,3 @@
-console.log('deez nuts');
 $(document).ready(function() {
     $('#viewFrame a').click(function(event) {
         $('#linkHistory').prepend('<li><a href="' + $(this).attr('href') + '">' + $(this).text() + '</a></li>');
